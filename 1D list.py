27beee45888df5f53452fbdfde7decc08207cf8c
jw05266634-crypto <jw05266634@priestley.ac.uk>
2025-10-21 11:01:05 +0100

    Add files via upload

---
+++ 1D list.py
@@ -0,0 +1,15 @@
+numbers = [10, 20, 30,40]
+
+#adds a number to the end of a list
+numbers.append(50)
+
+#adds a number to anywhere in a list
+numbers.insert(4, 60)
+
+#removes a specific value
+numbers.remove(50)
+
+#removes a value based on index
+del numbers[0]
+
+print(numbers)
